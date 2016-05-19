eb2c017922735de5aec874cc768fe7d613fb366c
soukingang <soukingang@gmail.com>
2016-05-19 14:35:04 +0800

    Update connection_manager.py for python3
    Update connection_manager.py for python3

--- src/DatabaseLibrary/connection_manager.py
+++ src/DatabaseLibrary/connection_manager.py
@@ -12,7 +12,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-import ConfigParser
+import configparser
 from robot.api import logger
 import sys
 
