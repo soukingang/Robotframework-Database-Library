deb978ef12ddb4723f67699ab6e6b6ce5d5e0c73
soukingang <soukingang@gmail.com>
2016-05-19 14:31:12 +0800

    Update setup.py for python3
    Update setup.py for python3

--- setup.py
+++ setup.py
@@ -17,23 +17,23 @@
 
 """Setup script for Robot's DatabaseLibrary distributions"""
 
-from distutils.core import setup
-
 import sys
-from os.path import join, dirname
-sys.path.append(join(dirname(__file__), 'src'))
+from os.path import abspath, dirname, join
 from ez_setup import use_setuptools
-use_setuptools()
 from setuptools import setup
-execfile(join(dirname(__file__), 'src', 'DatabaseLibrary', 'version.py'))
+
+sys.path.append(join(dirname(__file__), 'src'))
+use_setuptools()
+version_file = join(dirname(__file__), 'src', 'DatabaseLibrary', 'VERSION.py')
+exec(compile(open(version_file).read(), version_file, 'exec'))
 
 def main():
     setup(name         = 'robotframework-databaseslibrary',
           version      = VERSION,
           description  = 'Database utility library for Robot Framework',
-          author       = 'qitaos',
-          author_email = 'qitaos@gmail.com',
-          url          = 'https://github.com/qitaos/Robotframework-Database-Library/',
+          author       = 'soukingang',
+          author_email = 'soukingang@gmail.com',
+          url          = 'https://github.com/soukingang/Robotframework-Database-Library/',
           package_dir  = { '' : 'src'},
           packages     = ['DatabaseLibrary'],
           package_data={'DatabaseLibrary': ['VERSION']},
